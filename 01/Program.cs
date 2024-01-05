0a2683ae7ebc7b640866e42d1bde5c6d6d4cf4c0
Dimitri Marion <dimitri.marion@gmail.com>
2024-01-05 15:50:48 +0100

    day 1

---
+++ 01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+Dictionary<string, string> digitMapping = new Dictionary<string, string>
+{
+    { "one", "1" },
+    { "two", "2" },
+    { "three", "3" },
+    { "four", "4" },
+    { "five", "5" },
+    { "six", "6" },
+    { "seven", "7" },
+    { "eight", "8" },
+    { "nine", "9" }
+};
+
+string path = "./input.txt";
+if (File.Exists(path))
+{
+    string[] readText = File.ReadAllLines(path);
+    int calibrationsSum = 0;
+    foreach (string line in readText)
+    {
+        Console.WriteLine(line);
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sbDigits = new StringBuilder();
+        foreach (char c in line)
+        {
+            int number;
+            bool isInt = int.TryParse(new string([c]), out number);
+            if (isInt)
+            {
+                sb.Append(c);
+                sbDigits.Clear();
+            }
+            else
+            {
+                sbDigits.Append(c);
+                string sbDigitsStr = sbDigits.ToString();
+
+                foreach (var pair in digitMapping)
+                {
+                    if (sbDigitsStr.Contains(pair.Key))
+                    {
+                        sb.Append(pair.Value);
+                        sbDigits.Clear();
+                        sbDigits.Append(c);
+                    }
+                }
+            }
+        }
+        string numbers = sb.ToString();
+        Console.WriteLine(numbers);
+        string calibration = new string([numbers[0], numbers[numbers.Length - 1]]);
+        int calibrationInt = Int32.Parse(calibration);
+        calibrationsSum += calibrationInt;
+        Console.WriteLine(calibration);
+        Console.WriteLine("");
+    }
+    Console.WriteLine(calibrationsSum);
+}
---
+++ 01/README.md
@@ -0,0 +1,21 @@
+# Advent of Code #1
+
+link: https://adventofcode.com/2023/day/1
+
+## C# learned
+
+- Open and read file
+- String manipulation, StringBuilder
+- convert string to int with TryParse and Parse
+- Dictionary
+
+## Resources
+
+- [Strings - C# Programming Guide - C# | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/strings/)
+- [How to determine whether a string represents a numeric value - C# Programming Guide - C# | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/strings/how-to-determine-whether-a-string-represents-a-numeric-value)
+- [How to convert a string to a number - C# Programming Guide - C# | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number)
+- [How to concatenate multiple strings (C# Guide) - C# | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/csharp/how-to/concatenate-multiple-strings)
+- [StringBuilder Class (System.Text) | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/api/system.text.stringbuilder?view=net-8.0)
+- [Int32.Parse Method (System) | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/api/system.int32.parse?view=net-8.0)
+- [Int32.TryParse Method (System) | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/api/system.int32.tryparse?view=net-8.0)
+- [Dictionary<TKey,TValue> Class (System.Collections.Generic) | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.dictionary-2?view=net-8.0)
