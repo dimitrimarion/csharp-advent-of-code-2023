b6dc18490791ddf13cfdd9bbbedf915bfbcd5a00
Dimitri Marion <dimitri.marion@gmail.com>
2024-01-05 18:56:47 +0100

    day 2 part 1

--- 02/Program.cs
+++ 02/Program.cs
@@ -2,3 +2,48 @@
 
 string[] input = FileLibrary.ReadInput("./input.txt");
 FileLibrary.PrintInput(input);
+
+Dictionary<string, int> cubesInBag = new Dictionary<string, int>
+{
+    {"red", 12},
+    {"green", 13},
+    {"blue", 14}
+};
+
+int gameIndex = 1;
+int gameIndexSum = 0;
+foreach (string line in input)
+{
+    //    Console.WriteLine(String.Join("|", line.Split(":")));
+    string[] cubesSet = line.Split(":")[1].Split(";");
+    Console.WriteLine(String.Join("|", cubesSet));
+    bool gameValid = true;
+    foreach (string set in cubesSet)
+    {
+        if (!gameValid)
+            break;
+
+        string[] colorsSet = set.Split(",");
+        Console.WriteLine(String.Join("|", colorsSet));
+        foreach (string color in colorsSet)
+        {
+            string[] cube = color.Trim().Split();
+            string cubeColor = cube[1];
+            int cubeValue = int.Parse(cube[0]);
+            Console.WriteLine(String.Join("|", cube));
+            int cubeColorValue;
+            if (cubesInBag.TryGetValue(cubeColor, out cubeColorValue))
+            {
+                if (cubeValue > cubeColorValue)
+                {
+                    gameValid = false;
+                    break;
+                }
+            }
+        }
+    }
+    if (gameValid)
+        gameIndexSum += gameIndex;
+    gameIndex++;
+}
+Console.WriteLine($"Result {gameIndexSum}");
