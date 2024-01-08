b2cfb4695a91b7004fdd9b618c473d102fa9153a
Dimitri Marion <dimitri.marion@gmail.com>
2024-01-08 17:05:10 +0100

    day 4

---
+++ 04/Program.cs
@@ -0,0 +1,45 @@
+﻿using UtilsLibrary;
+
+string[] input = FileLibrary.ReadInput("./input.txt");
+//FileLibrary.PrintInput(input);
+
+double result = 0;
+int index = 0;
+List<int> cards = Enumerable.Repeat(1, input.Length).ToList();
+
+foreach (string line in input)
+{
+    string[] numbers = line.Split(":")[1].Split("|");
+    string winningNumbers = numbers[0];
+    string yourNumbers = numbers[1];
+    int[] winningNumbersArray = winningNumbers.Trim()
+                                              .Split()
+                                              .Where(el => !string.IsNullOrEmpty(el))
+                                              .Select(el => int.Parse(el)).ToArray();
+    int[] yourNumbersArray = yourNumbers.Trim()
+                                        .Split()
+                                        .Where(el => !string.IsNullOrEmpty(el))
+                                        .Select(el => int.Parse(el)).ToArray();
+
+    HashSet<int> winningNumbersSet = new HashSet<int>(winningNumbersArray);
+    HashSet<int> yoursNumbersSet = new HashSet<int>(yourNumbersArray);
+    winningNumbersSet.IntersectWith(yoursNumbersSet);
+
+    if (winningNumbersSet.Count > 0)
+    {
+        result += Math.Pow(2, winningNumbersSet.Count - 1);
+    }
+
+    for (int i = 0; i < cards[index]; i++)
+    {
+        for (int j = 0; j < winningNumbersSet.Count; j++)
+        {
+            cards[index + 1 + j] += 1;
+        }
+    }
+    index++;
+}
+
+int totalCards = cards.Sum();
+Console.WriteLine($"Result part 1: {result}");
+Console.WriteLine($"Result part 2: {totalCards}");
---
+++ 04/README.md
@@ -0,0 +1,16 @@
+# Advent of code day 4
+
+link: https://adventofcode.com/2023/day/4
+
+## C#/.Net learned
+
+- `HashSet` and `IntersectWith` method to get only elements in both sets.
+- Create a list filled with 1 with `Enumerable.Repeat`.
+- Using Linq `Enumerable.Select` and `Enumerable.Where`, "equivalent" to `map` and `filter` in javascript.
+
+# Resources
+
+- [Enumerable.Select Method (System.Linq) | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.select?view=net-8.0)
+- [Enumerable.Where Method (System.Linq) | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.where?view=net-8.0)
+- [HashSet<T> Class (System.Collections.Generic) | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.hashset-1?view=net-8.0)
+- [Enumerable.Repeat<TResult>(TResult, Int32) Method (System.Linq) | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.repeat?view=net-8.0)
